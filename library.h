b8771710043bd7ef268340e781de9f1835385092
KRVPerera <rukshan.viduranga@gmail.com>
2022-09-27 21:44:11 +0530

    basic project files

---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef SEEDRMAN_LIBRARY_H
+#define SEEDRMAN_LIBRARY_H
+
+void hello();
+
+#endif //SEEDRMAN_LIBRARY_H
---
+++ test/integration_tests.cpp
@@ -0,0 +1,41 @@
+//
+// Created by ruksh on 9/27/2022.
+//
+#include <catch2/catch_test_macros.hpp>
+#include <curl/curl.h>
+
+unsigned int Factorial( unsigned int number ) {
+    return number <= 1 ? number : Factorial(number-1)*number;
+}
+
+
+TEST_CASE( "simple get", "[curl]" )
+{
+    CURL *curl;
+    CURLcode res;
+
+    curl = curl_easy_init();
+    if(curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "https://example.com");
+        /* example.com is redirected, so we tell libcurl to follow redirection */
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+        /* Perform the request, res will get the return code */
+        res = curl_easy_perform(curl);
+        /* Check for errors */
+        if(res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                    curl_easy_strerror(res));
+
+        /* always cleanup */
+        curl_easy_cleanup(curl);
+    }
+}
+
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( Factorial(1) == 1 );
+//    REQUIRE( Factorial(2) == 2 );
+//    REQUIRE( Factorial(3) == 6 );
+//    REQUIRE( Factorial(10) == 3628800 );
+}
+

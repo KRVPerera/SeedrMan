eb785d90d1160bcfd4fd918e792108f7a99f4fc8
KRVPerera <rukshan.viduranga@gmail.com>
2022-10-23 11:07:06 +0530

    build: fix build

--- test/integration_tests.cpp
+++ test/integration_tests.cpp
@@ -5,6 +5,8 @@
 #include <curl/curl.h>
 #include <iostream>
 
+#include "library.h"
+
 unsigned int Factorial( unsigned int number ) {
     return number <= 1 ? number : Factorial(number-1)*number;
 }
@@ -36,6 +38,14 @@ TEST_CASE( "simple get", "[curl]" )
 
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
     REQUIRE( Factorial(1) == 1 );
+//    REQUIRE( Factorial(2) == 2 );
+//    REQUIRE( Factorial(3) == 6 );
+//    REQUIRE( Factorial(10) == 3628800 );
+}
+
+TEST_CASE( "Test Library", "[hello]" ) {
+    hello();
+REQUIRE( Factorial(1) == 1 );
 //    REQUIRE( Factorial(2) == 2 );
 //    REQUIRE( Factorial(3) == 6 );
 //    REQUIRE( Factorial(10) == 3628800 );
